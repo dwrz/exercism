931b7618a4964d77652c294cf527db890cb4be61
dwrz <dwrz@dwrz.net>
2019-01-06 21:13:09 +0000

    Add elisp/hello-world

---
+++ elisp/hello-world/hello-world.el
@@ -0,0 +1,15 @@
+;;; hello-world.el --- Hello World Exercise (exercism)
+
+;;; Commentary:
+;; This package provides a Hello World function.
+
+;;; Code:
+
+
+(provide 'hello-world)
+
+(defun hello ()
+  "Hello, World!"
+  )
+
+;;; hello-world.el ends here

d43303ce4aaa5e21c99deaec2aa0fcde213aad75
dwrz <dwrz@dwrz.net>
2019-04-22 01:16:56 +0000

    Remove grains/validSquare

--- c/grains/src/grains.c
+++ c/grains/src/grains.c
@@ -1,10 +1,5 @@
 #include "grains.h"
 
-static int validSquare(int n) {
-  if (n >= 1 && n <= 64) return 1;
-  return 0;
-}
-
 unsigned long long square(int n) {
   if (!validSquare(n)) return 0;
   if (n == 1) return 1;

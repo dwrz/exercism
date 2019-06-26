e50959488ae2ed6f7aaa234be983e6786167499c
dwrz <dwrz@dwrz.net>
2019-06-26 08:04:32 +0000

    Refactor c/binary_search header

--- c/binary-search/src/binary_search.h
+++ c/binary-search/src/binary_search.h
@@ -1,1 +1,12 @@
+#ifndef BINARY_SEARCH_H
+#define BINARY_SEARCH_H
+
+// binary_search expects:
+// val: an integer value to search
+// arr[]: an array of *sorted* integers
+// len: the length of ARR[]
+// If val is found, the function returns a pointer to an array index.
+// Otherwise, it returns NULL.
 int* binary_search(int val, int arr[], int len);
+
+#endif // BINARY_SEARCH_H
